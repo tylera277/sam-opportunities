b0b38a34b5ed3eff4f1d6998d6e325fe7a4596d8
Tyler <tylera277@gmail.com>
2026-03-15 13:56:24 -0600

    Updating main code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+# Define the pages
+main_page = st.Page('main_page.py', title='Main Page')
+raw_data_exploration = st.Page('raw_data_exploration.py', title='Raw Data Exploration')
+opportunities_dashboard = st.Page('opportunities_dashboard.py', title='Opportunities Dashboard')
+
+
+# Set up navigation
+pg = st.navigation([main_page, raw_data_exploration, opportunities_dashboard])
+
+pg.run()
